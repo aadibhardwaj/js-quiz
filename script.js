2312bd510cc0ea701eebec6b8e572d7ddf228c1f
Aadi <aadibhardwaj93@gmail.com>
2025-03-24 19:17:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// let timer = 5;
+
+const options = document.querySelector()
+
+// function startTimer() {
+//     interval = setInterval(() => {
+//         if (timer > 0) {
+//             timer--;
+//             timerDisplay.textContent = timer;
+//         } else {
+//             clearInterval(interval);
+//             if (!isAnswered) {
+//                 showResult(false);
+//             }
+//         }
+//     }, 1000);
+// }
+
+// var sec = 30;
+// var timer = setInterval(function(){
+//     document.getElementById('timerDisplay').innerHTML = '00:' + sec;
+//     sec--;
+//     if (sec < 0) {
+//         clearInterval(timer);
+//     }
+// }, 1000);
+
+// const questions =[]
+// function questions ()
+// {
+    
+// }
+
+
+
+
+
+
+
